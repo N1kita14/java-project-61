4c3f63b54c4f75eaa89dc9ea9e0150b8bb63aa2d
N1kita14 <nikita141299.n@yandex.ru>
2024-06-06 20:24:12 +0300

    Правки Even

--- app/src/main/java/hexlet/code/game/Even.java
+++ app/src/main/java/hexlet/code/game/Even.java
@@ -2,7 +2,7 @@ package hexlet.code.game;
 import java.util.Scanner;
 
 public class Even {
-    public static void playEven(String[] args) {
+    public static void playEven() {
         Scanner s = new Scanner(System.in);
         String number = s.nextLine();
         if (Integer.parseInt(number) % 2 == 0) {

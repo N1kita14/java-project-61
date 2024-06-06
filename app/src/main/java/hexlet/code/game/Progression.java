5b552b85acfec3d9e1c0f3867bb2ad2abdf2d740
N1kita14 <nikita141299.n@yandex.ru>
2024-06-06 19:02:04 +0300

    +Progression

--- app/src/main/java/hexlet/code/game/Progression.java
+++ app/src/main/java/hexlet/code/game/Progression.java
@@ -1,4 +1,48 @@
 package hexlet.code.game;
 
+import hexlet.code.Tools;
+import hexlet.code.Engine;
+
 public class Progression {
+    static final String QUESTION = "What number is missing in the progression?";
+
+    static final int MAX_STEP = 5;
+    static final int MIN_NUMBERS = 5;
+    static final int MAX_ROW = 10;
+    static final int MAX_FIRST = 1;
+
+    public static void start() {
+        Engine.run(QUESTION, progressionDataGenerate());
+    }
+
+    static String[] progressionGenerate(int first, int len, int step) {
+        String[] numbers = new String[len];
+        numbers[0] = Integer.toString(first);
+        for (var i = 1; i < numbers.length; i++) {
+            numbers[i] = Integer.toString(Integer.parseInt(numbers[i - 1]) + step);
+        }
+        return numbers;
+    }
+
+    static String[][] progressionDataGenerate() {
+        String[][] data = new String[Engine.REPEAT][2];
+
+        for (var i = 0; i < Engine.REPEAT; i++) {
+            int step = Tools.random(1, MAX_STEP);
+            int len = Tools.random(MIN_NUMBERS, MAX_ROW);
+            int first = Tools.random(1, MAX_FIRST);
+            int unknown = Tools.random(0, len - 1);
+
+            var progression = progressionGenerate(first, len, step);
+
+            String result = progression[unknown];
+            progression[unknown] = "..";
+            String question = String.join(" ", progression);
+
+            data[i][0] = question;
+            data[i][1] = result;
+        }
+        return data;
+    }
+}
 }

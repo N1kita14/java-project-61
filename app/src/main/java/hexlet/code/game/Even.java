38772e8ab0a6e2234ac3db781502099424cc2643
N1kita14 <nikita141299.n@yandex.ru>
2024-06-06 22:37:26 +0300

    fix Even

--- app/src/main/java/hexlet/code/game/Even.java
+++ app/src/main/java/hexlet/code/game/Even.java
@@ -1,15 +1,31 @@
 package hexlet.code.game;
-import java.util.Scanner;
+
+import hexlet.code.Tools;
+import hexlet.code.Engine;
 
 public class Even {
-    public static void playEven() {
-        Scanner s = new Scanner(System.in);
-        String number = s.nextLine();
-        if (Integer.parseInt(number) % 2 == 0) {
-            System.out.println("Число " + number + " четное");
+    static final String DESCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+    static final int MAX_RANDOM_RANGE = 100;
+
+    public static void start() {
+        Engine.run(DESCRIPTION, evenGenerate());
+    }
+
+    static String[][] evenGenerate() {
+        String[][] data = new String[Engine.REPEAT][2];
+        for (var i = 0; i < Engine.REPEAT; i++) {
+            int generatedNumber = Tools.random(1, MAX_RANDOM_RANGE);
+
+            String result = isEven(generatedNumber) ? "yes" : "no";
+            String question = Integer.toString(generatedNumber);
+
+            data[i][0] = question;
+            data[i][1] = result;
         }
-        else {
-            System.out.println("Число " + number +" нечетное");
-        }
+        return data;
+    }
+
+    static boolean isEven(int input) {
+        return (input % 2 == 0);
     }
 }
